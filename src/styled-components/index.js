3c9c708d98761895ec12072d4ac70a8248d6e07a
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 01:26:04 +0700

    styled components

---
+++ src/styled-components/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    margin-top: 2rem;
+  }
+`
+
+export const HeaderContainer = styled.div`
+  h2 {
+    color: #96979c;
+  }
+`
