57ef967fd9b7a9aaa2a00a3d2ee7d219d08d312f
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 01:26:32 +0700

    modified main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { CenterContainer, HeaderContainer } from "../styled-components"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <CenterContainer>
+      <HeaderContainer>
+        <h1>Rayhan Wirjowerdojo</h1>
+        <h2>Full-stack Web Developer</h2>
+      </HeaderContainer>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/blog/">Blog</Link>
+    </CenterContainer>
   </Layout>
 )
 
