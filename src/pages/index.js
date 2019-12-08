ace17e18945c30294bf9b2105af7923e0f1022a3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 20:35:47 +0700

    content styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { CenterContainer, HeaderContainer } from "../styled-components"
+import {
+  CenterContainer,
+  HeaderTextContainer,
+  HeaderContainer
+} from "../styled-components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <CenterContainer>
       <HeaderContainer>
-        <h1>Rayhan Wirjowerdojo</h1>
-        <h2>Full-stack Web Developer</h2>
-      </HeaderContainer>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <HeaderTextContainer>
+          <h1>Rayhan Wirjowerdojo</h1>
+          <h2>Full-stack Web Developer</h2>
+        </HeaderTextContainer>
         <Image />
-      </div>
-      <Link to="/blog/">Blog</Link>
+      </HeaderContainer>
     </CenterContainer>
   </Layout>
 )
