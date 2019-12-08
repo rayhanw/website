84a8be5c5212a3b4e8a2bc68b944ec1a731c7cee
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 18:21:06 +0700

    added dynamically create pages method for blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,25 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blogTemplate.js")
+  const result = await graphql(`
+    query {
+      allContentfulBlog {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allContentfulBlog.edges.forEach(({ node }) => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${node.slug}`,
+      context: { slug: node.slug }
+    })
+  })
+}
