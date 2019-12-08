f77a268fde9e5217a3869af968b543dfacfd155c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 23:13:21 +0700

    short description of me

---
+++ src/components/description.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import { Section } from "../styled-components"
+
+const Description = () => {
+  return (
+    <Section>
+      <p>
+        Hi! I'm Rayhan and I am a fullstack web developer based in Bali, also a
+        teaching assistant for{" "}
+        <a href="https://www.lewagon.com/bali" target="_blank">
+          Le Wagon Bali
+        </a>
+        .
+      </p>
+    </Section>
+  )
+}
+
+export default Description
