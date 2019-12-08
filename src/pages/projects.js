c3198e1584563e8346e04116515300c9f24052bb
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 23:14:18 +0700

    added SEO tag for title

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import ProjectsCollection from "../components/projects/projects"
+import SEO from "../components/seo"
 import Layout from "../components/layout"
 import { ProjectGrid } from "../styled-components"
 
 const Projects = () => {
   return (
     <Layout>
+      <SEO title="Projects" />
       <h3>Projects that I've worked on</h3>
       <ProjectGrid>
         <ProjectsCollection />
