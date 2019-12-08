392657e8ef53fa6de855e81b41eadfae9f0a6793
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-12-08 18:23:37 +0700

    styling

--- src/styled-components/index.js
+++ src/styled-components/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { Link } from "gatsby"
 
 export const CenterContainer = styled.div`
   display: flex;
@@ -12,5 +13,59 @@ export const CenterContainer = styled.div`
 export const HeaderContainer = styled.div`
   h2 {
     color: #96979c;
+  }
+`
+
+export const BlogBox = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 2rem;
+  width: 100%;
+  background-color: rgba(200, 200, 200, 0.1);
+  margin-bottom: 1rem;
+  border-radius: 4px;
+  transition: background-color 0.25s ease;
+  text-decoration: none;
+  &:hover {
+    background-color: rgba(200, 200, 200, 0.3);
+  }
+  > span {
+    margin-bottom: 1rem;
+  }
+  > a {
+    margin-bottom: 0.4rem;
+  }
+  > h3 {
+    margin: 0;
+    margin-bottom: 0.5rem;
+  }
+`
+
+export const HeaderBox = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: rgba(255, 255, 255, 0.2);
+  margin-bottom: 1rem;
+  > ul {
+    display: inline-block;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    > li {
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      margin-right: 0.5rem;
+    }
+  }
+`
+
+export const StyledLink = styled(Link)`
+  transition: color 0.25s ease;
+  &:hover {
+    color: var(--textHoverLink);
   }
 `
