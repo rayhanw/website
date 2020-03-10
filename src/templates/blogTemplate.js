68404bb29261f6f9174941889a9ffb735f0f8963
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-03-10 22:46:44 +0800

    switch to dangerous set inner html

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 
@@ -9,8 +8,10 @@ export const query = graphql`
     contentfulBlog(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
-      body {
-        json
+      content {
+        childMarkdownRemark {
+          html
+        }
       }
     }
   }
@@ -18,21 +19,16 @@ export const query = graphql`
 
 const BlogTemplate = ({ data }) => {
   const { contentfulBlog } = data
-  const options = {
-    renderNode: {
-      "embedded-asset-block": node => {
-        const alt = node.data.target.fields.title["en-US"]
-        const src = node.data.target.fields.file["en-US"].url
-        return <img alt={alt} src={src} height={300} width={300} />
-      }
-    }
-  }
 
   return (
     <Layout>
       <h1>{contentfulBlog.title}</h1>
       <p>{contentfulBlog.publishedDate}</p>
-      {documentToReactComponents(contentfulBlog.body.json, options)}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: contentfulBlog.content.childMarkdownRemark.html
+        }}
+      />
       <Link to="/blog">Back</Link>
     </Layout>
   )
