09e9182b0a3c8cff19879e2d5a4085c8bc389076
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-03-10 23:12:16 +0800

    vscode remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,33 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     },
-    `gatsby-transformer-remark`
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-vscode`,
+            // All options are optional. Defaults shown here.
+            options: {
+              theme: "Dark+ (default dark)", // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: "", // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [], // Third-party extensions providing additional themes and languages
+              languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({
+                // Function allowing dynamic setting of additional class names on individual lines
+                content, //   - the string content of the line
+                index, //   - the zero-based index of the line within the code fence
+                language, //   - the language specified for the code fence
+                meta //   - any options set on the code fence alongside the language (more on this later)
+              }) => "",
+              logLevel: "warn" // Set to 'info' to debug if something looks wrong
+            }
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
