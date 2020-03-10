3687d6c4b8ddfd0f9480adbe823332f185eb1520
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-03-10 22:46:35 +0800

    changed icon and added markdown transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/headshot.jpg` // This path is relative to the root of the site.
       }
     },
     {
@@ -39,7 +39,8 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
-    }
+    },
+    `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
